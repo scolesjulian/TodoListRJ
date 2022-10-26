358484aee144523c833df2645aed0e0c1f1f1e28
Julian Scoles <scolesjulian1@gmail.com>
2022-10-25 23:37:53 -0300

    Refactorizacion y creacion de mas components

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,82 @@
-import React, { Fragment, useState, useRef, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { TodoList } from "./components/TodoList";
 
+import './App.css';
+import Header from "./MyComponents/Header";
+import {Todos} from "./MyComponents/Todos";
+import {Footer} from "./MyComponents/Footer";
+import {AddTodo} from "./MyComponents/AddTodo";
+import {About} from "./MyComponents/About";
+import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router, 
+  Switch, //switch basically matches the path one by one , and if it match it renders the component.
+  Route
+} from "react-router-dom";
 
-const KEY = "todoApp.todos";
-//funcion normal
-export function App() {
-  const todoTaskRef = useRef();
-  //aca se cambia el state, si se cambia el state siempre se hace copia del anterior state
-  const [todos, setTodos] = useState([
-    { id: 1, task: "Tarea ", completed: false },
-  ]);
-  // funciones manejadoras de eventos con arrow function para
-  // comprobamos si ya hay todos almacenados
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(KEY));
-    if (storedTodos) {
-      setTodos(storedTodos);
+function App() {
+  let initTodo;
+  if(localStorage.getItem("todos") === null){
+    initTodo = [];
+  }
+  else{
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+  const onDelete = (todo)=>{
+    console.log("I am ondelete of todo",todo);
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
+    
+  }
+
+  const addTodo = (title, desc) =>{
+    console.log("Adding this todo",title,desc);
+    let Sno;
+    if(todos.length===0){
+      Sno = 1;
     }
-  }, []);
-  // para tener persistencia que se mantengan las tareas al recargar salvamos el array de todos
-  // localStaorage del navegador solo almacena string
+    else {
+    Sno = todos[todos.length - 1].Sno + 1;
+    }
+    const myTodo = {
+      Sno: Sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+    
+  }
+  const [todos, setTodos] = useState(initTodo);
   useEffect(() => {
-    localStorage.setItem(KEY, JSON.stringify(todos));
-  }, [todos]);
-  // aca maneja el checkbox, hace copia del state anterior de todo, lo modifica y lo vuelve a enviar
-  const toggleTodo = (id) => {
-    const newTodos = [...todos];
-    const todo = newTodos.find((todo) => todo.id === id);
-    todo.completed = !todo.completed;
-    setTodos(newTodos);
-  };
-
-  const handleTodoAdd = (event) => {
-    const task = todoTaskRef.current.value;
-    if (task === "") return;
-    // seteamos anterior state
-    setTodos((prevTodos) => {
-      return [...prevTodos, { id: uuidv4(), task, completed: false }];
-    });
-
-    todoTaskRef.current.value = null;
-  };
-  // eliminar
-  const handleClearAll = () => {
-    const newTodos = todos.filter((todo) => !todo.completed);
-    setTodos(newTodos);
-  };
-
+    localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos])
+  
   return (
-    // componente padre
-    //ref recat identifica que componente se esta usando
-    <Fragment>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoTaskRef} type="text" placeholder="Nueva tarea" />
-      <button style = {{color:'green'}} onClick={handleTodoAdd}>+</button>
-      <button style = {{color:'red'}} onClick={handleClearAll}>-</button>
-      <div>
-        <h5>
-        Te quedan {todos.filter((todo) => !todo.completed).length} tareas por
-        terminar
-        </h5>
-      </div>
-    </Fragment>
+    <>
+    <Router>
+    <Header title= " My Todos List" searchBar={false}/>
+    <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+            <>
+              <AddTodo addTodo={addTodo}/>
+              <Todos todos={todos} onDelete={onDelete}/>
+            </>)
+          }}>
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          
+          
+        </Switch>
+    
+    <Footer/>
+    </Router>
+    </>
+
   );
 }
 
-// las props se pasan de raiz al mas pequeno
-//los eventos de mas abajo a mas arriba
+export default App;
---
+++ src/MyComponents/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div align="center">
+            <h3> About </h3>
+            <p>This app lets you write, organize, and<br/>
+             reprioritize your tasks more efficiently.</p>
+             <p>This app lets you write, organize, and<br/>
+             reprioritize your tasks more efficiently.</p>
+             <p>This app lets you write, organize, and<br/>
+             reprioritize your tasks more efficiently.</p>
+             <p>This app lets you write, organize, and<br/>
+             reprioritize your tasks more efficiently.</p>
+             <p>This app lets you write, organize, and<br/>
+             reprioritize your tasks more efficiently.</p>
+             <p>This app lets you write, organize, and<br/>
+             reprioritize your tasks more efficiently.</p>
+             <p>This app lets you write, organize, and<br/>
+             reprioritize your tasks more efficiently.</p>
+             <p>This app lets you write, organize, and<br/>
+             reprioritize your tasks more efficiently.</p>
+             <p>This app lets you write, organize, and<br/>
+             reprioritize your tasks more efficiently.</p>
+        </div>
+    )
+}
+
+
